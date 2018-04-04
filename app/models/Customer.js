e8ed3eb63b2f98782f6ace33af9bab08e5bb6802
Slade Utley <slade.utley@gmail.com>
2018-04-04 09:48:49 -0500

    able to get all customer information from database

--- app/models/Customer.js
+++ app/models/Customer.js
@@ -1,10 +1,22 @@
 'use strict';
-const { Database } = require('sqlite3').verbose();
-const { setActiveCustomer, getActiveCustomer } = require('./activeCustomer');
+// const { Database } = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./bangazon.sqlite');
+
+const { setActiveCustomer, getActiveCustomer } = require('../activeCustomer');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, '..', 'db', 'bangazon.sqlite'));
+// const db = new Database(path.join(__dirname, '..', 'db', 'bangazon.sqlite'));
 
-db.get(`SELECT * FROM customers`, (customer) => {
-  console.log(customer)
-});
+// db.get(`SELECT * FROM customers`, (customer) => {
+//   console.log(customer)
+// });
+
+module.exports.getAll = () => {
+  return new Promise((resolve, reject) => {
+    db.all(`SELECT * FROM customer`, (err, cust) => {
+      if (err) return reject(err);
+      resolve(cust);
+    });
+  });
+};

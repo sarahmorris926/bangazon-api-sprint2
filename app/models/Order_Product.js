91eda525b252991b67e3e7253a66554ce14659de
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-04-07 16:30:25 -0500

    second test for posting an order product, plus most of the post order product function

--- app/models/Order_Product.js
+++ app/models/Order_Product.js
@@ -23,7 +23,12 @@ module.exports.getOneOrderProduct = (id) => {
     });
 };
 
-module.exports.postOneOrderProduct = () => {
-
+module.exports.postOneOrderProduct = ({quantity, order_id, product_id, price}) => {
+    return new Promise((resolve, reject) => {
+        db.run(`INSERT into order_product VALUES(null, ${quantity}, ${order_id}, ${product_id}, ${price})`, function(err, order) {
+            if (err) return reject(err);
+            resolve({line_id: this.lastID});
+        })
+    })
 }
 
--- test/order_product.test.js
+++ test/order_product.test.js
@@ -1,10 +1,15 @@
 
 const { assert: { equal, deepEqual, isFunction, isObject, isArray } } = require("chai");
 const { getAllOrderProducts, postOneOrderProduct, getOneOrderProduct } = require("../app/models/Order_Product")
-
+const createOrderProductTable = require("../db/order_product_table");
 // Order_Product Model
 // get All Order Products
 describe("get order products", () => {
+    after(done => {
+        createOrderProductTable().then(() => {
+            done();
+        });
+    });
     describe("get all order products", () => {
         it("should be a function", () => {
             isFunction(getAllOrderProducts);
@@ -27,13 +32,12 @@ describe("get order products", () => {
             isFunction(getOneOrderProduct)
         })
         it("should return an object", () => {
-            getOneOrderProduct(1).then(op =>{
+            getOneOrderProduct(1).then(op => {
                 isObject(op);
             })
         })
         it("should return an object with the correct Line ID", () => {
             getOneOrderProduct(1).then(op => {
-                console.log(op);
                 equal(1, op.line_id)
             })
         })
@@ -44,6 +48,18 @@ describe("get order products", () => {
 describe("post one order proudct", () => {
     it("should be a function", () => {
         isFunction(postOneOrderProduct);
+    })
+    it("should return the Line ID of the object posted", () => {
+        let newOP = {
+            quantity: 10,
+            order_id: 5,
+            product_id: 6,
+            price: 5.00
+        }
+        postOneOrderProduct(newOP).then(op => {
+            console.log(op);
+            equal(141, op.line_id);
+        })
     })
 })
 

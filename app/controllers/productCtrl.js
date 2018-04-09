a099970f7803f2189bfe0f8cafb18d716c4d1b92
Courtney Brothers <courtneywbrothers@gmail.com>
2018-04-09 13:55:05 -0500

    error handler for if no products are in order

--- app/controllers/productCtrl.js
+++ app/controllers/productCtrl.js
@@ -14,15 +14,18 @@ const {
 
 module.exports.completeAPayment = (customerId) => {
   getOrder(customerId).then((orderId) => {
-      console.log(orderId[0].order_id, "order Id");
-      getSumOfProducts(orderId[0].order_id).then((sum) => {
-        console.log("sum", sum);
+    getSumOfProducts(orderId[0].order_id).then((sum) => {
+
+      if (sum[0]['sum (price)'] === 0) {
+        console.log("Please add products to your order")
+      } else {
+
         let headerDivider = `${magenta('*********************************************************')}`
         return new Promise((resolve, reject) => {
           console.log(`
-    ${headerDivider}
-    ${magenta(`** Your Current Order Total is ${sum[0]['sum (price)']}, ready to purhase?**`)}
-    ${headerDivider}`)
+      ${headerDivider}
+      ${magenta(`** Your Current Order Total is ${sum[0]['sum (price)']}, ready to purhase?**`)}
+      ${headerDivider}`)
           prompt.get([{
             name: 'choice',
             description: '(Y/N)',
@@ -30,59 +33,60 @@ module.exports.completeAPayment = (customerId) => {
             message: "please select Y or N"
           }], function (err, results) {
             if (err) return reject(err);
-            if (results.choice === "Y"){
-              getPaymentMethods(customerId).then((payment)=>{
+            if (results.choice === "Y") {
+              getPaymentMethods(customerId).then((payment) => {
                 console.log(payment.length);
 
-                  if (payment.length === 0) {
-                    console.log("please add payments to your account")
-                  } else {
+                if (payment.length === 0) {
+                  console.log("please add payments to your account")
+                } else {
 
 
-                    return new Promise((resolve, reject)=>{
-                      console.log(`${headerDivider}${(`Choose a payment option`)}${headerDivider}`);
-              
+                  return new Promise((resolve, reject) => {
+                    console.log(`${headerDivider}${(`Choose a payment option`)}${headerDivider}`);
 
-                    for (i = 0; i < payment.length; i++){
+
+                    for (i = 0; i < payment.length; i++) {
                       console.log(`${i+1} . ${payment[i].payment_option}`);
                     };
 
-                      prompt.get([{
-                        name:"Choice",
-                        description: `enter number of the payment you would like to select`,
-                        type: 'integer',
-                        message:'enter number of payment you would like to select'
-                      }], function (err,results){
-                        if (err) return reject(err);
-        
-                        if (results.Choice === 1){
-                          updatePaymentMethod(payment[0].payment_id,orderId[0].order_id)
-                          .then((data) =>{
+                    prompt.get([{
+                      name: "Choice",
+                      description: `enter number of the payment you would like to select`,
+                      type: 'integer',
+                      message: 'enter number of payment you would like to select'
+                    }], function (err, results) {
+                      if (err) return reject(err);
+
+                      if (results.Choice === 1) {
+                        updatePaymentMethod(payment[0].payment_id, orderId[0].order_id)
+                          .then((data) => {
                             console.log("payment completed")
                           })
-                        } else if (results.Choice === 2){
-                          updatePaymentMethod(payment[1].payment_id,orderId[0].order_id)
-                          .then((data) =>{
+                      } else if (results.Choice === 2) {
+                        updatePaymentMethod(payment[1].payment_id, orderId[0].order_id)
+                          .then((data) => {
                             console.log("payment completed")
                           })
 
-                        } else {
-                          updatePaymentMethod(payment[2].payment_id,orderId[0].order_id)
-                          .then((data) =>{
+                      } else {
+                        updatePaymentMethod(payment[2].payment_id, orderId[0].order_id)
+                          .then((data) => {
                             console.log("payment completed")
                           })
-                        }
-                      })
-
+                      }
                     })
 
-                  }
-                })
+                  })
+
+                }
+              })
             } else {
               console.log("You chose to not complete your order!")
             }
           });
         });
-      })
+      }
     })
+  })
 }

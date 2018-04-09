a31865a01381cb524b05e13ef17a70dbcc621188
Courtney Brothers <courtneywbrothers@gmail.com>
2018-04-09 12:32:34 -0500

    update order with order id correctly assigning order_id and payment_id

--- app/controllers/productCtrl.js
+++ app/controllers/productCtrl.js
@@ -34,25 +34,29 @@ module.exports.completeAPayment = (customerId) => {
               getPaymentMethods(customerId).then((payment)=>{
                 return new Promise((resolve, reject)=>{
                   console.log(`${headerDivider}${(`Choose a payment option`)}${headerDivider}`)
+                  console.log(payment, "payment");
 
                   for (i = 0; i < payment.length; i++){
+                    console.log("payment", payment);
                     console.log(`${i+1} . ${payment[i].payment_option}`);
                   }
                     prompt.get([{
                       name:"Choice",
-                      description: `enter name of payment you would like to select`,
-                      type: 'string',
-                      message:'enter name of payment you would like to select'
+                      description: `enter number of the payment you would like to select`,
+                      type: 'integer',
+                      message:'enter number of payment you would like to select'
                     }],function (err,results){
                       if (err) return reject(err);
-                      updatePaymentMethod(20, 11).then((data) =>{
-                        console.log("payment completed");
-                      })
-                      .catch((err) => {
-                        console.log((err), "error in updating payment method");
-                      })
+                      console.log('results.Choice', results.Choice);
+                      if (results.Choice === 1){
+                        updatePaymentMethod(payment[0].payment_id,orderId[0].order_id).then((data) =>{
+                          console.log("payment completed")
+                        })
+                        .catch((err) =>{
+                          console.log((err), "error in updating payment")
+                        })
+                      }
                     })
-                
                 })
               })
               .catch((err) =>{
--- app/models/Product.js
+++ app/models/Product.js
@@ -65,7 +65,7 @@ module.exports.getSumOfProducts = (orderId) =>{
 
 module.exports.getPaymentMethods = (customerId) => {
     return new Promise ((resolve,reject) => {
-        db.all(`SELECT payment_option from payment_type where customer_id = ${customerId}`, (err,sum) =>{
+        db.all(`SELECT payment_option, payment_id from payment_type where customer_id = ${customerId}`, (err,sum) =>{
             if (err) return reject(err);
             resolve(sum)
         })

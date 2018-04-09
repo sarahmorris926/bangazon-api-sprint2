405c46e9dc36fe8c1531f5e3274ed5c67ab0ed4a
Courtney Brothers <courtneywbrothers@gmail.com>
2018-04-09 13:15:39 -0500

    prompt customer to add payments to their account if there are none

--- app/controllers/productCtrl.js
+++ app/controllers/productCtrl.js
@@ -32,36 +32,52 @@ module.exports.completeAPayment = (customerId) => {
             if (err) return reject(err);
             if (results.choice === "Y"){
               getPaymentMethods(customerId).then((payment)=>{
-                return new Promise((resolve, reject)=>{
-                  console.log(`${headerDivider}${(`Choose a payment option`)}${headerDivider}`)
-                  console.log(payment, "payment");
+                console.log(payment.length);
 
-                  for (i = 0; i < payment.length; i++){
-                    console.log("payment", payment);
-                    console.log(`${i+1} . ${payment[i].payment_option}`);
-                  }
-                    prompt.get([{
-                      name:"Choice",
-                      description: `enter number of the payment you would like to select`,
-                      type: 'integer',
-                      message:'enter number of payment you would like to select'
-                    }],function (err,results){
-                      if (err) return reject(err);
-                      console.log('results.Choice', results.Choice);
-                      if (results.Choice === 1){
-                        updatePaymentMethod(payment[0].payment_id,orderId[0].order_id).then((data) =>{
-                          console.log("payment completed")
-                        })
-                        .catch((err) =>{
-                          console.log((err), "error in updating payment")
-                        })
-                      }
+                  if (payment.length === 0) {
+                    console.log("please add payments to your account")
+                  } else {
+
+
+                    return new Promise((resolve, reject)=>{
+                      console.log(`${headerDivider}${(`Choose a payment option`)}${headerDivider}`);
+              
+
+                    for (i = 0; i < payment.length; i++){
+                      console.log(`${i+1} . ${payment[i].payment_option}`);
+                    };
+
+                      prompt.get([{
+                        name:"Choice",
+                        description: `enter number of the payment you would like to select`,
+                        type: 'integer',
+                        message:'enter number of payment you would like to select'
+                      }], function (err,results){
+                        if (err) return reject(err);
+        
+                        if (results.Choice === 1){
+                          updatePaymentMethod(payment[0].payment_id,orderId[0].order_id)
+                          .then((data) =>{
+                            console.log("payment completed")
+                          })
+                        } else if (results.Choice === 2){
+                          updatePaymentMethod(payment[1].payment_id,orderId[0].order_id)
+                          .then((data) =>{
+                            console.log("payment completed")
+                          })
+
+                        } else {
+                          updatePaymentMethod(payment[2].payment_id,orderId[0].order_id)
+                          .then((data) =>{
+                            console.log("payment completed")
+                          })
+                        }
+                      })
+
                     })
+
+                  }
                 })
-              })
-              .catch((err) =>{
-                console.log(err,"error in getPaymentMethods")
-              })
             } else {
               console.log("You chose to not complete your order!")
             }
@@ -69,7 +85,4 @@ module.exports.completeAPayment = (customerId) => {
         });
       })
     })
-    .catch((err) => {
-      console.log("err", err)
-    });
 }

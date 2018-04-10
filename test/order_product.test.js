ec802657114f8ec83604beeab8374b07c5f630c3
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-04-10 09:45:06 -0500

    added catches to test promises

--- test/order_product.test.js
+++ test/order_product.test.js
@@ -1,7 +1,7 @@
 
 const { assert: { equal, deepEqual, isFunction, isObject, isArray, isNumber } } = require("chai");
 const { getAllOrderProducts, postOneOrderProduct, getOneOrderProduct, getLastOrderProduct } = require("../app/models/Order_Product");
-const { addProductToOrder } = require("../app/controllers/order_productCtrl"); 
+const { addProductToOrder } = require("../app/controllers/order_productCtrl");
 const createOrderProductTable = require("../db/order_product_table");
 
 
@@ -16,12 +16,18 @@ describe("get order products", () => {
         it("should return an array", () => {
             getAllOrderProducts().then(data => {
                 isArray(data);
+            })
+            .catch((err) => {
+                console.log('Get All Order Products Should Return an Array', err);
             });
         });
 
         it("should be an object in the 0 index of the array", () => {
             getAllOrderProducts().then(data => {
                 isObject(data[0]);
+            })
+            .catch((err) => {
+                console.log('Get All Order Products Should be an object in the 0 index of the array', err);
             });
         });
     });
@@ -32,11 +38,17 @@ describe("get order products", () => {
         it("should return an object", () => {
             getOneOrderProduct(1).then(op => {
                 isObject(op);
+            })
+            .catch((err) => {
+                console.log('Get One Order Product Should Return an object', err);
             });
         });
         it("should return an object with the correct Line ID", () => {
             getOneOrderProduct(1).then(op => {
                 equal(1, op.line_id)
+            })
+            .catch((err) => {
+                console.log('Get One Order Product Should Return an object with the correct Line Id', err);
             });
         });
     });
@@ -47,6 +59,9 @@ describe("get order products", () => {
         it("should return an integer", () => {
             getLastOrderProduct().then(op => {
                 isNumber(op);
+            })
+            .catch((err) => {
+                console.log('Get last Line ID Should Return an Integer', err);
             });
         });
     });
@@ -71,6 +86,9 @@ describe("post one order proudct", () => {
     it("should return the Line ID of the object posted", () => {
         postOneOrderProduct(newOP).then(op => {
             equal(141, op.line_id);
+        })
+        .catch((err) => {
+            console.log('Post Post Order Product Should Return the Line ID of the object posted', err);
         });
     });
     it("should return the object posted when called by the Line ID", () => {
@@ -84,6 +102,9 @@ describe("post one order proudct", () => {
         return postOneOrderProduct(newOP).then(op => {
             return getOneOrderProduct(141).then(postedObj => {
                 deepEqual(postedObj, expected);
+            })
+            .catch((err) => {
+                console.log('Post One Order Products Should Return the expected object', err);
             });
         });
     });
@@ -96,7 +117,7 @@ describe("Final Order Product Function", () => {
             isObject(product);
         })
             .catch((err) => {
-            console.log('error 2', err);
+                console.log('Final Order Product Function should return an object', err);
             });
     })
     it("should add an object to the table that can be queried", () => {
@@ -113,6 +134,9 @@ describe("Final Order Product Function", () => {
                 })
             })
         })
+            .catch((err) => {
+                console.log('Final Order Product Function Should return the expected object', err);
+            });
     })
 })
 
